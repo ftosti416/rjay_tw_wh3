c241d26320f490841693779cd62dc99ee0a78cee
rJay <federico.tosti@outlook.com>
2023-03-23 00:21:35 +0100

    added comments and added iterations for cultures

--- rjay_recruitment_main/rjay_recruitment_main.lua
+++ rjay_recruitment_main/rjay_recruitment_main.lua
@@ -66,28 +66,16 @@ local wh_agents = {
     ------------------------------------------------------------------------------------------
 
 }
-local brt_main_factions = {
-    "wh2_dlc14_brt_chevaliers_de_lyonesse", "wh_main_brt_carcassonne", "wh_main_brt_bordeleaux", "wh_main_brt_bretonnia"
+-- COUNTING ONLY MAIN FACTION SINCE USUALLY THE ONLY ONE TO CONFEDERATE
+-- MINOR FACTION WITH UNIQUE AGENTS ARE ALSO ADDED TO THE CULTURE TABLE
+local culture = {
+ ["brt"] = {"wh2_dlc14_brt_chevaliers_de_lyonesse", "wh_main_brt_carcassonne", "wh_main_brt_bordeleaux", "wh_main_brt_bretonnia"},
+ ["cth"] = {"wh3_main_cth_the_northern_provinces", "wh3_main_cth_the_northern_provinces"},
+ ["hef"] = {"wh2_main_hef_order_of_loremasters", "wh2_main_hef_eataine", "wh2_main_hef_avelorn", "wh2_main_hef_nagarythe","wh2_main_hef_yvresse", "wh2_dlc15_hef_imrik"},
+ ["ksl"] = {"wh3_main_ksl_the_great_orthodoxy", "wh3_main_ksl_the_ice_court", "wh3_main_ksl_ursun_revivalists"},
+ ["emp"] = {"wh_main_emp_empire","wh2_dlc13_emp_golden_order", "wh3_main_emp_cult_of_sigmar", "wh2_dlc13_emp_the_huntmarshals_expedition","wh_main_emp_middenland"},
+ ["wef"] = {"wh_dlc05_wef_wood_elves", "wh_dlc05_wef_argwylon", "wh2_dlc16_wef_sisters_of_twilight", "wh2_dlc16_wef_drycha"}
 }
-local cth_main_factions = {
-    "wh3_main_cth_the_northern_provinces", "wh3_main_cth_the_northern_provinces"
-}
-local hef_main_factions = {
-    "wh2_main_hef_order_of_loremasters", "wh2_main_hef_eataine", "wh2_main_hef_avelorn", "wh2_main_hef_nagarythe",
-    "wh2_main_hef_yvresse", "wh2_dlc15_hef_imrik"
-}
-local ksl_main_factions = {
-    "wh3_main_ksl_the_great_orthodoxy", "wh3_main_ksl_the_ice_court", "wh3_main_ksl_ursun_revivalists"
-}
-local emp_main_factions = {
-    "wh_main_emp_empire",
-    "wh2_dlc13_emp_golden_order", "wh3_main_emp_cult_of_sigmar", "wh2_dlc13_emp_the_huntmarshals_expedition",
-    "wh_main_emp_middenland"
-}
-local wef_main_factions = {
-    "wh_dlc05_wef_wood_elves", "wh_dlc05_wef_argwylon", "wh2_dlc16_wef_sisters_of_twilight", "wh2_dlc16_wef_drycha"
-}
-
 controller = {
     processed = false,
 }
@@ -97,22 +85,28 @@ end
 
 _G.get_controlleer = get_controlleer
 
+--SETS LISTENERS AS PROCESSED
 function controller:init()
     self.processed = true
 end
 
+--MAIN FUNCITION
 function controller:main()
+    --STARTS LISTENER ONLY IF NEW GAME OR NEVER PROCESSED
     if cm:is_new_game() or not self.processed then
         controller:init_listeners();
     end
 end
 
 function controller:init_listeners()
+    --STARTS THE AGENTS CHECK TO SEE IF ALREADY UNLOCKED
     rjay_recruitment_listener();
+    --STARTS THE CULTURE CHECK TO SEE IF CULTURE IS "DEAD" (FOR CONFEDERATED AGENTS)
     rjay_culture_eradicated_listener();
     self:init();
 end
 
+--GENERATES AGENT LISTENER
 function rjay_recruitment_listener()
     core:add_listener("rjay_faction_controller", "FactionTurnStart", function(context)
         return context:faction():is_human()
@@ -130,20 +124,19 @@ function rjay_recruitment_listener()
     end, true);
 end
 
+--GENERATES CULTURE LISTENER
 function rjay_culture_eradicated_listener()
     core:add_listener("rjay_culture_controller", "FactionTurnStart", function(context)
         return context:faction():is_human()
     end, function(context)
         local player_faction = cm:model():world():faction_by_key(context:faction():name());
-        check_culture_alive(brt_main_factions, player_faction);
-        check_culture_alive(cth_main_factions, player_faction);
-        check_culture_alive(hef_main_factions, player_faction);
-        check_culture_alive(ksl_main_factions, player_faction);
-        check_culture_alive(emp_main_factions, player_faction);
-        check_culture_alive(wef_main_factions, player_faction);
+        for index, table in ipairs(culture) do
+            check_culture_alive(table, player_faction);
+        end
     end, true)
 end
 
+--CHECKS IF CULTURE IS COMPLETLY "DEAD" (PLAYER DOES NOT COUNT) AND IF AGENT IS ALREADY UNLOCKED
 ---@param culture_table table
 ---@param player_faction FACTION_SCRIPT_INTERFACE
 function check_culture_alive(culture_table, player_faction)
@@ -167,6 +160,8 @@ function check_culture_alive(culture_table, player_faction)
     end
 end
 
+
+--CHECKS IF AGENT ALREADY ON PLAYER ROSTER. IF NOT AND PLAYER OWN AGENT REGION WILL CREATE A DILEMMA
 ---@param player_faction FACTION_SCRIPT_INTERFACE
 ---@param agent table
 function check_agent(player_faction, agent)
@@ -185,7 +180,7 @@ function check_agent(player_faction, agent)
         end
     end
 end
-
+--CHECKS PLAYER REGIONS TO SEE IF AGENT MAIN REGION IS OWNED BY PLAYER AND IF IS NOT UNDER SIEGE
 ---@param player_faction FACTION_SCRIPT_INTERFACE
 ---@param agent table
 function check_region(player_faction, agent)
@@ -201,6 +196,7 @@ function check_region(player_faction, agent)
     return player_own;
 end
 
+--CREATES DILEMMA FOR AGENT RECRUITMENT
 ---@param player_faction FACTION_SCRIPT_INTERFACE
 ---@param agent table
 function create_recruitment_dilemma(player_faction, agent)
@@ -216,13 +212,13 @@ function create_recruitment_dilemma(player_faction, agent)
     dilemma:add_choice_payload("SECOND", dilemma_second);
 
     cm:launch_custom_dilemma_from_builder(dilemma, player_faction);
-    --CREATE LISTENER
+    --CREATES LISTENER
     core:add_listener("rjay_" .. agent.subtype .. "_unlocked", "DilemmaChoiceMadeEvent", function(context)
         return context:dilemma() == "rjay_recruitment_dilemma_" .. agent.subtype;
     end, function(context)
         local choice = context:choice_key();
         if choice == "FIRST" then
-            --ACCEPTED
+            --IF ACCEPTED ADD AGENT TO POOL
             cm:spawn_character_to_pool(player_faction:name(), agent.name, agent.lastname, "", "", 50, true, agent.type,
                 agent.subtype, true, "");
         end
